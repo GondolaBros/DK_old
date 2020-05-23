79f16615dd7d2be71e69bdaf555566c1daa89d43
Kyle Jensen <kyle.jensen72@gmail.com>
2020-05-22 21:57:51 -0400

    Adding gitignore

---
+++ Assets/Shine/Client/AI/AgentModeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentModeController : MonoBehaviour
+{
+    //List of desires corresponding to each mode
+    //Evaluates each desire and selects the corresponding mode
+    //
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Shine/Client/Controllers/BotController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : MonoBehaviour
+{
+    //Look at match information, player information, and map information (opponent information only within viewport)
+    //
+
+    
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
