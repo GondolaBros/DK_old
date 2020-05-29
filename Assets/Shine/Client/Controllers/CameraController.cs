8535601a40eb6fd8e778700acc4ef96e71e38476
Kyle Jensen <kyle.jensen72@gmail.com>
2020-05-29 00:16:11 -0400

    First revision camera controller, follows player and clamps to camera width

--- Assets/Shine/Client/Controllers/CameraController.cs
+++ Assets/Shine/Client/Controllers/CameraController.cs
@@ -12,9 +12,19 @@ public class CameraController : MonoBehaviour
     private float maxZoom = -2f;
     private float minZoom = -15f;
 
+    private float orbitXRotation = -20f;
+    private float orbitYRotation = -180f;
+
+    private Vector3 cameraOffset;
+
     private Transform target;
 
-
+    private void Awake()
+    {
+        this.target = GameObject.FindGameObjectWithTag("Player").transform.Find("CameraTarget");
+        this.transform.rotation = Quaternion.Euler(65f, 90f, 0f);
+        this.cameraOffset = new Vector3(-2.774f, 5.14f, 0f);
+    }
 
     private void Update()
     {
@@ -26,31 +36,11 @@ public class CameraController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (target == null)
-        {
-            GameObject playerObject;
-            if ((playerObject = GameObject.FindGameObjectWithTag("Player")) != null)
-            {
-                target = playerObject.transform.Find("CameraTarget");
-                //MoveToTarget();
-                //camera collider logic
-            }
-            return;
-        }
-
-
-
- 
-
-
         //camera collider logic
+        Vector3 newPosition = target.position + cameraOffset;
+        newPosition.z = Mathf.Clamp(newPosition.z, 10f, 30f);
+        transform.position = newPosition;
     }
-
-    private void MoveToTarget()
-    {
-
-    }
-
 
     private void HandleZoom()
     {
