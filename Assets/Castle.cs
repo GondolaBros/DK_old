e2396ed8a6e18bc0da8d6633fdd7958feb9b1d70
Kyle Jensen <kyle.jensen72@gmail.com>
2020-06-26 00:30:55 -0400

    Adding projectile file to gitignore

--- Assets/Castle.cs
+++ Assets/Castle.cs
@@ -7,7 +7,7 @@ public class Castle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
--- Assets/Shine/Client/CameraController1.cs
+++ Assets/Shine/Client/CameraController1.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 
 public class CameraController1 : MonoBehaviour
 {
